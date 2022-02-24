87e150aced317e04b93363bd01e200f4d1156b27
JangWonJong <jwj9922@naver.com>
2022-02-24 16:32:58 +0900

    추가추가

--- components/Bmi.js
+++ components/Bmi.js
@@ -1,10 +1,24 @@
 import React, { useState }  from "react";
 import Layout from "../containers/Layout";
+import axios from 'axios';
 
 export default function Bmi () {
-    const [name, setName] = useState("")
-    const [height, setHeight] = useState(0)
-    const [weight, setWeight] = useState(0)
+    const [name, setName] = useState('')
+    const [height, setHeight] = useState(0.0)
+    const [weight, setWeight] = useState(0.0)
+    const getBmi = async (e) => {
+        e.preventDefault()
+        alert(`버튼 클릭`)
+        try{            
+            await axios.get(`http://localhost:8080/member/bmi/홍길동/180.5/80.5`)
+            .then((res)=>{
+                alert(`답장이 도착했습니다 [내용] ${res}`)
+            })
+
+        }catch(error){
+            alert(`답장이 도착했습니다`)
+        }
+    }
 
     const print = () => {
        let name = document.getElementById('name').value
@@ -35,6 +49,7 @@ export default function Bmi () {
                     <button onClick={()=>{print()}}>입력</button>
                     
                     <div>결과:{name}, {height}, {weight}</div>
+                    <input type="button" onClick={getBmi} value="BMI 체크"/><br/> 
                 </div>
            
         </Layout>
